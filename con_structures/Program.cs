3d4f724d15a235d0a52659b54c670de35ecd4bfc
AJ <amariusj.lesure@gmail.com>
2025-05-01 18:54:43 -0500

    Replace Program.cs

--- con_structures/Program.cs
+++ con_structures/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+					
+public class Program
+{
+	public void travelMode() {
+		string mode;
+		Console.WriteLine("Select mode of travel: Bus, Train, or Flight");
+		mode = Console.ReadLine();
+		
+		switch(mode) {
+			case "Bus":
+				Console.WriteLine("Booking bus ticket.");
+				break;
+			case "Train":
+				Console.WriteLine("Booking train ticket.");
+				break;
+			case "Flight":
+				Console.WriteLine("Booking plane ticket.");
+				break;
+			default:
+				Console.WriteLine("Invalid selection. Please choose Bus, Train, or Flight.");
+				break;
+		}
+	}
+		
+	public void getDiscount() {
+		int age;
+		Console.WriteLine("Input user age:");
+		age = int.Parse(Console.ReadLine());
+
+		if (age < 12) {
+			Console.WriteLine("Half price ticket.");
+		} else if (age <= 65) {
+			Console.WriteLine("Full price ticket.");
+		} else {
+			Console.WriteLine("Senior discount ticket.");
+		}
+	}
+	
+	public static void Main()
+	{
+		Program p = new Program();
+		p.travelMode();
+		p.getDiscount();
+	}
+}
